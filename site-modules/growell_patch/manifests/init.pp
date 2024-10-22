a5e4e2c40ea96bc83b6885bca3c5b5639c08b5c5
Garrett Rowell <garrett@puppet.com>
2024-10-22 09:39:17 -0600

    add cust patch

---
+++ site-modules/growell_patch/manifests/init.pp
@@ -0,0 +1,11 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include growell_patch
+class growell_patch {
+  class { 'patching_as_code':
+    classify_pe_patch => true
+  }
+}
