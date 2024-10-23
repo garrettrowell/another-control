1d9a2ff9f679f0fc52015ac0f1a6c2642d5baa55
Garrett Rowell <garrett@puppet.com>
2024-10-23 13:04:57 -0600

    yes

--- site-modules/growell_patch/manifests/init.pp
+++ site-modules/growell_patch/manifests/init.pp
@@ -16,16 +16,36 @@ class growell_patch (
   String $patch_group
 ) {
 
-  $test = growell_patch::patchday($patch_schedule['day'], $patch_schedule['week'], $patch_schedule['offset'])
-  notify { "patchday: ${test}": }
+  $patchday = growell_patch::patchday($patch_schedule['day'], $patch_schedule['week'], $patch_schedule['offset'])
+  $week_suffix = $patch_schedule['week'] ? {
+    1       => 'st',
+    2       => 'nd',
+    3       => 'rd',
+    default => 'th'
+  }
+  $patch_suffix = $patchday['count_of_week'] ? {
+    1       => 'st',
+    2       => 'nd',
+    3       => 'rd',
+    default => 'th'
+  }
+  notify {
+    default:
+      withpath => false,
+      ;
+    "Hieradata says we will patch ${patch_schedule['offset']} days after the ${patch_schedule['week']}${week_suffix} ${patch_schedule['day']}":
+      ;
+    "Which corresponds to the ${patchday['count_of_week']}${patch_suffix} ${patchday['day_of_week']}":
+      ;
+  }
 
   class { 'patching_as_code':
     classify_pe_patch => true,
     patch_group       => $patch_group,
     patch_schedule    => {
       $patch_group => {
-        day_of_week   => $test['day_of_week'],
-        count_of_week => $test['count_of_week'],
+        day_of_week   => $patchday['day_of_week'],
+        count_of_week => $patchday['count_of_week'],
         hours         => $patch_schedule['hours'],
         max_runs      => $patch_schedule['max_runs'],
         reboot        => $patch_schedule['reboot']
@@ -33,17 +53,3 @@ class growell_patch (
     }
   }
 }
-
-# @param [Hash] patch_schedule
-#   Hash of available patch_schedules. Default schedules are in /data/common.yaml of this module
-# @option patch_schedule [String] :day_of_week
-#   Day of the week to patch, valid options: 'Any', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
-# @option patch_schedule [Variant[Integer,Array[Integer]]] :count_of_week
-#   Which week(s) in the month to patch, use number(s) between 1 and 5
-#
-# @option patch_schedule [String] :hours
-#   Which hours on patch day to patch, define a range as 'HH:MM - HH:MM'
-# @option patch_schedule [String] :max_runs
-#   How many Puppet runs during the patch window can Puppet install patches. Must be at least 1.
-# @option patch_schedule [String] :reboot
-#   Reboot behavior, valid options: 'always', 'never', 'ifneeded'
