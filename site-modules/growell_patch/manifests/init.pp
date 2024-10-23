f908fa88a48df40d6bfd1e0c8cb91d6fd65305e4
Garrett Rowell <garrett@puppet.com>
2024-10-23 15:16:44 -0600

    this

--- site-modules/growell_patch/manifests/init.pp
+++ site-modules/growell_patch/manifests/init.pp
@@ -20,31 +20,66 @@ class growell_patch (
   # for example to achieve: 3 days after the 2nd Thursday.
   $patchday = growell_patch::patchday($patch_schedule['day'], $patch_schedule['week'], $patch_schedule['offset'])
 
-  # Pre-build the pre-patch command assuming one is set
-  $_pre_patch_path = $facts['kernel'] ? {
-    'Linux'   => [
-      '/usr/local/sbin',
-      '/usr/local/bin',
-      '/usr/sbin',
-      '/usr/bin',
-      '/sbin',
-      '/bin',
-    ],
-    'windows' => undef,
-  }
-  $_pre_patch_provider = $facts['kernel'] ? {
-    'Linux'   => undef,
-    'windows' => 'powershell',
-  }
-  $_pre_patch_commands = $pre_patch_script ? {
-    undef   => undef,
-    default => {
-      'prepatch script' => {
-        'command'  => epp("growell_patch/${pre_patch_script}"),
-        'path'     => $_pre_patch_path,
-        'provider' => $_pre_patch_provider,
+  case $facts['kernel'] {
+    'Linux': {
+      $_pre_patch_script_path = '/opt/puppetlabs/pe_patch/pre_patch_script.sh'
+
+      if $pre_patch_script == undef {
+        $_pre_patch_commands = undef
+        file { 'growell_patch pre_patch_script':
+          path   => $_pre_patch_script_path,
+          ensure => absent,
+        }
+      } else {
+        $_pre_patch_commands = {
+          'prepatch script' => {
+            'command' => $_pre_patch_script_path,
+            'path'    => [
+              '/usr/local/sbin',
+              '/usr/local/bin',
+              '/usr/sbin',
+              '/usr/bin',
+              '/sbin',
+              '/bin',
+            ],
+          }
+        }
+        file { 'growell_patch pre_patch_script':
+          ensure => present,
+          path   => $_pre_patch_script_path,
+          source => "puppet:///modules/growell_patch/${pre_patch_script}",
+          mode   => '0700',
+          owner  => 'root',
+          group  => 'root',
+        }
       }
     }
+    'windows': {
+      $_pre_patch_script_path = 'C:/ProgramData/PuppetLabs/pe_patch/pre_patch_script.ps1'
+
+      if $pre_patch_script == undef {
+        $_pre_patch_commands = undef
+        file { 'growell_patch pre_patch_script':
+          ensure => absent,
+          path   => $_pre_patch_script_path,
+        }
+
+      } else {
+        $_pre_patch_commands = {
+          'prepatch script' => {
+            'command'  => $_pre_patch_script_path,
+            'provider' => 'powershell',
+          }
+        }
+
+        file { 'growell_patch pre_patch_script':
+          ensure => present,
+          path   => $_pre_patch_script_path,
+          mode   => '0770',
+        }
+      }
+    }
+    default: { fail("Unsupported OS: ${facts['kernel']}") }
   }
 
   # Helpers only for the notify resources below
