569c92cc0cf417ec50b2a4af8a3963ffc0ec0b5f
Garrett Rowell <garrett@puppet.com>
2024-10-23 14:05:31 -0600

    pre-patch

--- site-modules/growell_patch/manifests/init.pp
+++ site-modules/growell_patch/manifests/init.pp
@@ -14,9 +14,38 @@ class growell_patch (
     reboot   => Enum['always', 'never', 'ifneeded'],
   }] $patch_schedule,
   String $patch_group,
+  Optional[String[1]] $pre_patch_script = undef,
 ) {
-
+  # function determines the patchday to set based on the given day, week and offset
+  # for example to achieve: 3 days after the 2nd Thursday.
   $patchday = growell_patch::patchday($patch_schedule['day'], $patch_schedule['week'], $patch_schedule['offset'])
+
+  # Pre-build the pre-patch command assuming one is set
+  $_pre_patch_path = $facts['kernel'] ? {
+    'Linux'   => [
+      '/usr/local/sbin',
+      '/usr/local/bin',
+      '/usr/sbin',
+      '/usr/bin',
+      '/sbin',
+      '/bin',
+    ],
+    'windows' => undef,
+  }
+  $_pre_patch_provider = $facts['kernel'] ? {
+    'Linux'   => undef,
+    'windows' => 'powershell',
+  }
+  $_pre_patch_commands = $pre_patch_script ? {
+    undef   => undef,
+    default => {
+      'prepatch script' => {
+        'command'  => epp("growell_patch/${pre_patch_script}"),
+        'path'     => $_pre_patch_path,
+        'provider' => $_pre_patch_provider,
+      }
+    }
+  }
 
   # Helpers only for the notify resources below
   $week_suffix = $patch_schedule['week'] ? {
@@ -45,9 +74,10 @@ class growell_patch (
   }
 
   class { 'patching_as_code':
-    classify_pe_patch => true,
-    patch_group       => $patch_group,
-    patch_schedule    => {
+    classify_pe_patch  => true,
+    patch_group        => $patch_group,
+    pre_patch_commands => $_pre_patch_commands,
+    patch_schedule     => {
       $patch_group => {
         day_of_week   => $patchday['day_of_week'],
         count_of_week => $patchday['count_of_week'],
