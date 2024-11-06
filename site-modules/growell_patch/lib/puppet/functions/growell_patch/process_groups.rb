c5f745dabfb647c201d7eebdb454f25c7ff3dcf3
Garrett Rowell <garrett@puppet.com>
2024-11-06 08:29:21 -0700

    logic

--- site-modules/growell_patch/lib/puppet/functions/growell_patch/process_groups.rb
+++ site-modules/growell_patch/lib/puppet/functions/growell_patch/process_groups.rb
@@ -207,8 +207,10 @@ Puppet::Functions.create_function(:'growell_patch::process_groups') do
   #   - it is 12hrs after when the patch window ends
   def patchday?(patch_group, patch_schedule, time_now)
     parsed_window = parse_window(patch_schedule, time_now)
-    is_before     = before?((parsed_window['start_time'] - (60*60*12)), parsed_window['current_time'])
-    is_after      = after?((parsed_window['end_time'] + (60*60*12)), parsed_window['current_time'])
+    is_before     = parsed_window['current_time'].between?((parsed_window['start_time'] - (60*60*12)), parsed_window['start_time'])
+    is_after      = parsed_window['current_time'].between?((parsed_window['end_time'] + (60*60*12)), parsed_window['end_time'])
+#    is_before     = before?((parsed_window['start_time'] - (60*60*12)), parsed_window['current_time'])
+#    is_after      = after?((parsed_window['end_time'] + (60*60*12)), parsed_window['current_time'])
     is_between    = in_window(parsed_window)
     is_before || is_after  || is_between
   end
