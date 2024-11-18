f6ce1c6466b48e3f1a56d03b3ea2878592cf3e18
Garrett Rowell <garrett@puppet.com>
2024-11-18 10:31:22 -0700

    moar

--- site-modules/growell_patch/plans/schedule_selfservice.pp
+++ site-modules/growell_patch/plans/schedule_selfservice.pp
@@ -6,12 +6,15 @@ plan growell_patch::schedule_selfservice(
 
   # manage fact file
   $results = apply($targets) {
-    notify { "factpath: ${facts['kernel']}": }
-    #    $fpath = join([split($facts['factpath'], ':')[0], 'growell_patch_override.json'], '/')
-    #    file { $fpath:
-    #      ensure  => present,
-    #      content => {'thing1' => 'imatest'},
-    #    }
+    $fdir = $facts['kernel'] ? {
+      'Linux'   => '/opt/puppetlabs/puppet/cache/lib/facter',
+      'windows' => 'C:/ProgramData/PuppetLabs/puppet/cache/lib/facter'
+    }
+    $fpath = join($fdir, 'growell_patch_override.json'], '/')
+    file { $fpath:
+      ensure  => present,
+      content => {'thing1' => 'imatest'},
+    }
   }
 
   $results.each |$result| {
