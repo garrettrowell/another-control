d822236e019e2dba3a0d90a1f25dd39c5ed9ed8f
Garrett Rowell <garrett@puppet.com>
2024-11-19 11:32:29 -0700

    plan

--- site-modules/growell_patch/plans/schedule_selfservice.pp
+++ site-modules/growell_patch/plans/schedule_selfservice.pp
@@ -4,7 +4,7 @@ plan growell_patch::schedule_selfservice(
   Integer $week,
   Integer $offset,
   String[1] $hours,
-  Optional[Enum['permanent','temporary']] $valid_for = 'temporary',
+  Optional[Enum['permanent','temporary']] $type = 'temporary',
   Optional[Integer] $max_runs = 1,
   Optional[String[1]] $reboot = 'ifneeded',
 ) {
@@ -17,23 +17,30 @@ plan growell_patch::schedule_selfservice(
   # manage fact file
   $results = apply($targets) {
     $fdir = "${facts['puppet_vardir']}/../../facter/facts.d"
-    $_valid = $valid_for ? {
-      'permanent' => 'permanent',
-      'temporary' => Timestamp.new(),
-    }
+    #    $_valid = $valid_for ? {
+    #      'permanent' => 'permanent',
+    #      'temporary' => Timestamp.new(),
+    #    }
     $fpath = join([$fdir, "${_override_fact}.json"], '/')
+    $cur_override = $facts[$_override_fact]
+    $has_permanent = $cur_override.dig('permanent') != undef
+    $has_temporary = $cur_override.dig('temporary') != undef
     file { $fpath:
       ensure  => present,
       content => to_json_pretty(
         {
           $_override_fact => {
-            'day'             => $day,
-            'week'            => $week,
-            'offset'          => $offset,
-            'hours'           => $hours,
-            'max_runs'        => $max_runs,
-            'reboot'          => $reboot,
-            'valid_for_month' => $_valid,
+            'temporary' => {
+              'day'             => $day,
+              'week'            => $week,
+              'offset'          => $offset,
+              'hours'           => $hours,
+              'max_runs'        => $max_runs,
+              'reboot'          => $reboot,
+              'valid_for_month' => $_valid,
+            },
+            'has_perm'  => $has_permanent,
+            'has_temp'  => $has_temporary
           }
         }
       ),
