1b780a74f3ab96648c1a54c125627360937ab45c
Garrett Rowell <garrett@puppet.com>
2024-11-21 09:27:43 -0700

    more

--- site-modules/growell_patch/lib/facter/growell_patch_scripts.rb
+++ site-modules/growell_patch/lib/facter/growell_patch_scripts.rb
@@ -1,5 +1,11 @@
 Facter.add('growell_patch_scripts') do
   setcode do
-    true
+    os = Facter.value('kernel')
+    script_base = os == 'windows' ? 'C:/ProgramData/PuppetLabs/pe_patch' : '/opt/puppetlabs/pe_patch'
+    script_ext  = os == 'windows' ? '.ps1' : '.sh'
+    {
+      'post_patch_script' => File.exist?("#{script_base}/post_patch_script#{script_ext}"),
+      'pre_patch_script' => File.exist?("#{script_base}/pre_patch_script#{script_ext}")
+    }
   end
 end
