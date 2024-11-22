2abe8fcc0882250bf7110f4387fbca5d7e26aa89
Garrett Rowell <garrett@puppet.com>
2024-11-22 10:57:04 -0700

    more

--- site-modules/growell_patch/lib/puppet/functions/growell_patch/process_groups.rb
+++ site-modules/growell_patch/lib/puppet/functions/growell_patch/process_groups.rb
@@ -34,7 +34,7 @@ Puppet::Functions.create_function(:'growell_patch::process_groups') do
 
     if patch_group.include? 'never'
       active_pg              = 'never'
-      call_function('create_resources', ['schedule', {'Growell_patch - Patch Window' => { 'period' => 'never'}}])
+      call_function('create_resources', 'schedule', {'Growell_patch - Patch Window' => { 'period' => 'never'}})
     elsif patch_group.include? 'always'
       bool_patch_day         = true
       active_pg              = 'always'
