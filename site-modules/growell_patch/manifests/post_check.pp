440e55bf74c64b63c9c0ec163066dfde3cb3f1f3
Garrett Rowell <garrett@puppet.com>
2024-12-02 17:07:36 -0700

    post_check bump

--- site-modules/growell_patch/manifests/post_check.pp
+++ site-modules/growell_patch/manifests/post_check.pp
@@ -5,16 +5,44 @@ class growell_patch::post_check (
   case $priority {
     'normal': {
       $_exec_title = 'post_check_script'
-      $_exec_schedule = 'Growell_patch - Patch Window'
+      $_schedule = 'Growell_patch - Patch Window'
+      $_notify_title_base = 'Growell_patch - Post Check'
     }
     'high': {
       $_exec_title = 'post_check_script (High Priority)'
-      $_exec_schedule = 'Growell_patch - High Priority Patch Window'
+      $_schedule = 'Growell_patch - High Priority Patch Window'
+      $_notify_title_base = 'Growell_patch - Post Check (High Priority)'
     }
   }
 
+  # Initially record that the post check failed
+  notify { "${_notify_title_base} - failed":
+    notify   => Exec[$_exec_title],
+    schedule => $_schedule,
+    message  => Deferred('growell_patch::reporting',
+    [
+      {
+        'post_check' => {
+          'status'    => 'failed',
+          'timestamp' => Timestamp.new()}}])
+  }
+
+  # Run the post check
   exec { $_exec_title:
-    schedule => $_exec_schedule,
+    schedule => $_schedule,
     *        => $exec_args
   }
+
+  # In the event of a failure this resource will get skipped
+  notify { "${_notify_title_base} - success":
+    require  => Exec[$_exec_title],
+    schedule => $_schedule,
+    message  => Deferred('growell_patch::reporting',
+    [
+      {
+        'post_check' => {
+          'status'    => 'success',
+          'timestamp' => Timestamp.new()}}])
+  }
+
 }
