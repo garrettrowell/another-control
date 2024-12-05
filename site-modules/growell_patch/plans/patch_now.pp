7048cd2ad91e961965b46e0923e259e7ce2ead01
Garrett Rowell <garrett@puppet.com>
2024-12-05 12:55:14 -0700

    more

--- site-modules/growell_patch/plans/patch_now.pp
+++ site-modules/growell_patch/plans/patch_now.pp
@@ -1,19 +1,20 @@
 plan growell_patch::patch_now(
   TargetSpec $targets,
+  Optional[Enum['always', 'never', 'ifneeded']] $pre_reboot = 'always',
+  Optional[Enum['always', 'never', 'ifneeded']] $post_reboot = 'always',
 ) {
   # collect facts
-  run_plan('facts', 'targets' => $targets)
+  run_plan('facts', 'targets' => $targets, '_catch_errors' => true)
+
+  out::message($targets)
 
   # patch now
-  $results = apply($targets) {
+  $results = apply($targets, '_description' => 'Main Patching Run', '_catch_errors' => true) {
     class { 'growell_patch':
       patch_group => 'always',
       run_as_plan => true,
     }
   }
-
-  # run the agent after patching
-  #  run_task('enterprise_tasks::run_puppet', $targets)
 
   # basic output
   $results.each |$result| {
