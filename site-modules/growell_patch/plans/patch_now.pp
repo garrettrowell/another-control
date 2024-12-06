92ab0ca26f4ca42038ec8f00829137370ffd7d46
Garrett Rowell <garrett@puppet.com>
2024-12-06 11:54:32 -0700

    more

--- site-modules/growell_patch/plans/patch_now.pp
+++ site-modules/growell_patch/plans/patch_now.pp
@@ -103,11 +103,11 @@ plan growell_patch::patch_now(
 
   # Determine for which nodes the pre_patching script (if it exists) ran successfully
   $pre_patching_script_success = $patch_resultset.ok_set.filter_set |$vals| {
-    'Exec[Growell_patch - Pre Patching Script - success]' in $vals['value']['report']['resource_statuses'].keys
+    'Exec[Growell_patch - Pre Patching Script - success]' in $vals.to_data['value']['report']['resource_statuses'].keys
   }
   # Determine for which nodes the pre_check ran successfully
   $pre_check_success = $patch_resultset.ok_set.filter_set |$vals| {
-    'Exec[Growell_patch - Pre Check - success]' in $vals['value']['report']['resource_statuses'].keys
+    'Exec[Growell_patch - Pre Check - success]' in $vals.to_data['value']['report']['resource_statuses'].keys
   }
 
   out::message("pre_patch_script_success: ${pre_patching_script_success.names}")
