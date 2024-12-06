8c15ffcc25bf758f505cb446f5cd187163139c5e
Garrett Rowell <garrett@puppet.com>
2024-12-06 10:27:36 -0700

    more

--- site-modules/growell_patch/plans/patch_now.pp
+++ site-modules/growell_patch/plans/patch_now.pp
@@ -57,7 +57,7 @@ plan growell_patch::patch_now(
     }
   }
 
-  $pre_reboot_success = $pre_reboot_resultset.ok_set
+  $pre_reboot_success = $pre_reboot_resultset.to_data
   $res = $pre_reboot_resultset.filter |$index, $vals| {
     'Reboot[Growell_patch - Pre Patch Reboot]' in $vals['value']['report']['resource_statuses'].keys and $vals['value']['report']['resource_statuses']['Reboot[Growell_patch - Pre Patch Reboot]']['changed'] == true
   }
